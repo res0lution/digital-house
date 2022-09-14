5224bf7aa5f28f199fd776abea5491b55a516bd1
Res0ution <maestro@hotger.com>
2022-09-14 15:46:56 +0300

    setup validation and hash

--- go.mod
+++ go.mod
@@ -15,7 +15,9 @@ require (
 	github.com/agext/levenshtein v1.2.1 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
 	github.com/go-openapi/inflect v0.19.0 // indirect
+	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/hashicorp/hcl/v2 v2.10.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
@@ -27,6 +29,7 @@ require (
 	github.com/valyala/fasthttp v1.40.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/zclconf/go-cty v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
 	golang.org/x/sys v0.0.0-20220909162455-aba9fc2a8ff2 // indirect
 	golang.org/x/text v0.3.7 // indirect
--- handlers/user.go
+++ handlers/user.go
@@ -1,12 +1,24 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
+	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
 	"github.com/gofiber/fiber/v2"
 	"github.com/res0lution/digital-house/ent/user"
 	"github.com/res0lution/digital-house/utils"
 )
+
+func (r registerRequest) validate() error {
+	return validation.ValidateStruct(&r, 
+		validation.Field(&r.FirstName, validation.Required, validation.Length(3, 20),),
+		validation.Field(&r.LastName, validation.Required, validation.Length(3, 20)),
+		validation.Field(&r.Email, validation.Required, is.Email),
+		validation.Field(&r.Password, validation.Required, validation.Length(6, 12)),
+	)
+}
 
 func (h *Handler) UserRegister(ctx *fiber.Ctx) error {
 	var request registerRequest
@@ -24,6 +36,15 @@ func (h *Handler) UserRegister(ctx *fiber.Ctx) error {
 		}
 	}
 
+	if err = request.validate(); err != nil {
+		err = ctx.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"error": true,
+			"message": err,
+		})
+
+		return nil
+	}
+
 	exists, _ := h.Client.User.Query().Where(user.Email(request.Email)).Only(ctx.Context())
 
 	if exists != nil {
@@ -33,12 +54,19 @@ func (h *Handler) UserRegister(ctx *fiber.Ctx) error {
 		})
 	}
 
+	hashpassword, err := utils.HashPassword(request.Password)
+
+	if err != nil {
+		fmt.Errorf("Failed hash user password: ", err)
+		return nil
+	}
+
 	_, err = h.Client.User.Create().
 		SetEmail(request.Email).
 		SetFirstName(request.FirstName).
 		SetLastName(request.LastName).
 		SetAvatar(request.Avatar).
-		SetPassword(request.Password).
+		SetPassword(hashpassword).
 		Save(ctx.Context())
 
 	if err != nil {
